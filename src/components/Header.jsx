47c34c2ed4ccdb5f510ad3cb551379e929f1eae0
Lafaiete Pedro <lafaietepedro3@gmail.com>
2024-12-02 23:37:39 -0300

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,42 @@
+import { useState } from "react";
 import "./Header.css";
 import arrowdown from "../assets/icon-arrow-down.svg";
 
 const Header = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <header className="header">
       <nav>
         <h1 className="logo">sunnyside</h1>
-        <ul className="menu">
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#services">Services</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-        </ul>
-        <button className="contact-btn">Contact</button>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`dropdown ${isDropdownOpen ? "open" : ""}`}>
+          <ul className="menu">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+          </ul>
+          <button className="contact-btn">Contact</button>
+        </div>
       </nav>
       <div className="hero">
         <div>
           <h2>WE ARE CREATIVES</h2>
-          <img src={arrowdown} alt="Arrow Down" />
+          <img className="slide-in-top" src={arrowdown} alt="Arrow Down" />
         </div>
       </div>
     </header>
@@ -30,3 +44,30 @@ const Header = () => {
 };
 
 export default Header;
+
+{
+  /* <header className="header">
+  <nav>
+    <h1 className="logo">sunnyside</h1>
+    <div className="hamburger" onClick="toggleMenu()">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <ul className="menu">
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Services</a>
+      </li>
+      <li>
+        <a href="#">Projects</a>
+      </li>
+      <li>
+        <button className="contact-btn">Contact</button>
+      </li>
+    </ul>
+  </nav>
+</header>; */
+}
